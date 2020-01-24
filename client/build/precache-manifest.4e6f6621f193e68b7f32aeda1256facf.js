d7901025beeca621d86bbf6cbec338fd1955f0d0
Brian O’Connor <brianoc707@gmail.com>
2020-01-24 15:16:08 -0500

    initial commit

---
+++ client/build/precache-manifest.4e6f6621f193e68b7f32aeda1256facf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48196c78b50214a3ba59862b540e895a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee14bfd57bc278d0eb6f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ee14bfd57bc278d0eb6f",
+    "url": "/static/js/2.564cfe20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.564cfe20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b771488a0f805467be4",
+    "url": "/static/js/main.62010aa6.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
